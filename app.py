97c7e17c14a67e0f6b48ba74b2c9b4027b487c48
farisulhaq <faries089@gmail.com>
2022-12-12 01:35:20 +0700

    add random forest

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from sklearn.cluster import KMeans
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
 
 
 # display
@@ -58,35 +59,46 @@ with dataframe:
         dt = pd.read_csv(
             'https://raw.githubusercontent.com/farisulhaq/dataset/main/bmi-dataset.csv')
         st.dataframe(dt)
+        # pisahkan fitur dan label
+        X = dt.drop('Index', axis=1)
+        y = dt['Index']
 
 
 with preporcessing:
     progress()
+    scaler = joblib.load("scaled.pkl")
     st.write('One Hot Prepocessing')
-    df = pd.get_dummies(dt, prefix='Gender')
-    st.dataframe(df)
+    df = pd.get_dummies(X, prefix='Gender')
+    scaled = scaler.fit_transform(df)
+    X = pd.DataFrame(scaled, columns=df.columns)
+    st.dataframe(X)
 
 
 with modeling:
     progress()
-    # pisahkan fitur dan label
-    X = df.drop('Index', axis=1)
-    y = df['Index']
     # split data
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, train_size=0.8, random_state=1)
-    mlpc, knc, dtc = st.tabs(
-        ["MLPClassifier", "KNeighborsClassifier", "DecisionTreeClassifier"])
-
+    rfc, mlpc, knc, dtc = st.tabs(
+        ["RandomForestClassifier", "MLPClassifier", "KNeighborsClassifier", "DecisionTreeClassifier"])
+    with rfc:
+        progress()
+        rf = joblib.load('Rf.pkl')
+        y_pred_rf = rf.predict(X_test)
+        akurasi_rf = accuracy_score(y_test, y_pred_rf)
+        label_rf = pd.DataFrame(
+            data={'Label Test': y_test, 'Label Predict': y_pred_rf}).reset_index(drop=True)
+        st.success(f'akurasi terhadap data test = {akurasi_rf}')
+        st.dataframe(label_rf)
     with mlpc:
         progress()
-        clf = joblib.load('clf.pkl')
-        y_pred_clf = clf.predict(X_test)
-        akurasi_clf = accuracy_score(y_test, y_pred_clf)
-        label_clf = pd.DataFrame(
-            data={'Label Test': y_test, 'Label Predict': y_pred_clf}).reset_index()
-        st.success(f'akurasi terhadap data test = {akurasi_clf}')
-        st.dataframe(label_clf)
+        mlp = joblib.load('mlp.pkl')
+        y_pred_mlp = mlp.predict(X_test)
+        akurasi_mlp = accuracy_score(y_test, y_pred_mlp)
+        label_mlp = pd.DataFrame(
+            data={'Label Test': y_test, 'Label Predict': y_pred_mlp}).reset_index(drop=True)
+        st.success(f'akurasi terhadap data test = {akurasi_mlp}')
+        st.dataframe(label_mlp)
     with knc:
         progress()
         knn = joblib.load('knn.pkl')
@@ -94,7 +106,7 @@ with modeling:
         y_pred_knn = knn.predict(X_test)
         akurasi_knn = accuracy_score(y_test, y_pred_knn)
         label_knn = pd.DataFrame(
-            data={'Label Test': y_test, 'Label Predict': y_pred_knn}).reset_index()
+            data={'Label Test': y_test, 'Label Predict': y_pred_knn}).reset_index(drop=True)
         st.success(f'akurasi terhadap data test = {akurasi_knn}')
         st.dataframe(label_knn)
     with dtc:
@@ -103,7 +115,7 @@ with modeling:
         y_pred_d3 = d3.predict(X_test)
         akurasi_d3 = accuracy_score(y_test, y_pred_d3)
         label_d3 = pd.DataFrame(
-            data={'Label Test': y_test, 'Label Predict': y_pred_d3}).reset_index()
+            data={'Label Test': y_test, 'Label Predict': y_pred_d3}).reset_index(drop=True)
         st.success(f'akurasi terhadap data test = {akurasi_d3}')
         st.dataframe(label_d3)
 
@@ -116,14 +128,9 @@ with implementation:
     gander = st.selectbox('Jenis Kelamin', ['Laki-Laki', 'Prempuan'])
     gander_female = 1 if gander == 'Prempuan' else 0
     gander_male = 1 if gander == 'Laki-Laki' else 0
-
+    model = st.selectbox("Model", ["Random Forest"])
     data = np.array([[height, weight, gander_female, gander_male]])
-    model = st.selectbox('Pilih Model', ['MLP', 'KNN', 'D3'])
-    if model == 'MLP':
-        y_imp = clf.predict(data)
-    elif model == 'KNN':
-        y_imp = knn.predict(data)
-    else:
-        y_imp = d3.predict(data)
-    st.success(f'Model yang dipilih = {model}')
-    st.success(f'Data Predict = {label[y_imp[0]]}')
+    data_scaled = scaler.transform(data)
+    if st.button("Predict"):
+        y_imp = rf.predict(data_scaled)
+        st.success(f'Data Predict = {label[y_imp[0]]}')
